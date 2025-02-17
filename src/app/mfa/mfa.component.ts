6674601c5022fbe8ceb438dbd32ef1e5f9411a44
andrei.kuksenak <andrei.kuksenak@kaseya.com>
2025-02-17 16:48:16 +0100

    add material

---
+++ src/app/mfa/mfa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mfa',
+  imports: [],
+  templateUrl: './mfa.component.html',
+  styleUrl: './mfa.component.scss'
+})
+export class MfaComponent {
+
+}
